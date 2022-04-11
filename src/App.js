3267b3d498f4009b7901caca60359b05e91ff5ed
zakimalikiakarinti <zaki.maliki@akarinti.tech>
2022-04-12 02:48:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 function BoilingVerdict(props) {
   if (props.celsius >= 100) {
     return <p>The water would boil.</p>;
@@ -6,27 +6,20 @@ function BoilingVerdict(props) {
   return <p>The water would not boil.</p>;
 }
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = { value: "" };
-  }
+function App(props) {
+  const [value, setState] = useState();
 
-  handleChange(e) {
-    this.setState({ value: e.target.value });
-  }
+  const handleChange = (e) => {
+    setState(e.target.value);
+  };
 
-  render() {
-    const value = this.state.value;
-    return (
-      <fieldset>
-        <legend>Enter temperature in Celsius:</legend>
-        <input value={value} onChange={this.handleChange} />
-        <BoilingVerdict celsius={parseFloat(value)} />
-      </fieldset>
-    );
-  }
+  return (
+    <fieldset>
+      <legend>Enter temperature in Celsius:</legend>
+      <input value={value} onChange={handleChange} />
+      <BoilingVerdict celsius={parseFloat(value)} />
+    </fieldset>
+  );
 }
 
 export default App;
